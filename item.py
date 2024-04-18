42f8497eafb36f8eb0fb7b3c86f241de9af1484c
Kazume <mcwicio@gmail.com>
2024-04-18 11:58:58 +0200

    English translation

--- item.py
+++ item.py
@@ -25,35 +25,35 @@ class Spell:
 
 
 warrior_weapons = [
-    Weapon("Miecz", 4, "Melee", 1, 50),
-    Weapon("Topór", 5, "Melee", 0.75, 60),
-    Weapon("Halabarda", 5, "Melee", 0.50, 90)
+    Weapon("Sword", 4, "Melee", 1, 50),
+    Weapon("Axe", 5, "Melee", 0.75, 60),
+    Weapon("Halberd", 5, "Melee", 0.50, 90)
 ]
 
 archer_weapons = [
-    Weapon("Łuk", 3, "Ranged", 1.75, 20),
-    Weapon("Kusza", 4, "Ranged", 1.3, 60),
-    Weapon("Pistolet", 5, "Ranged", 1.1, 110)
+    Weapon("Bow", 3, "Ranged", 1.75, 20),
+    Weapon("Crossbow", 4, "Ranged", 1.3, 60),
+    Weapon("Flintlock pistol", 5, "Ranged", 1.1, 110)
 ]
 
 mage_weapon = [
-    Weapon("Kostur", 4, "Magic", 1.2, 200)
+    Weapon("Magician's staff", 4, "Magic", 1.2, 200)
 ]
 
 warrior_armor = [
-    Armor("Kolczuga", 2, "Medium", 10, 100),
-    Armor("Płytówka", 3, "Medium", 5, 180),
-    Armor("Pantalony", 4, "Heavy", 1, 300)
+    Armor("Chainmail", 2, "Medium", 10, 100),
+    Armor("Chain armor", 3, "Medium", 5, 180),
+    Armor("Plate armor", 4, "Heavy", 1, 300)
 ]
 
 archer_armor = [
-    Armor("Klata", 0, "Light", 50, 0),
-    Armor("Kaptur", 1, "Light", 40, 20),
-    Armor("Skórzany kaftan", 2, "Light", 30, 40)
+    Armor("Chest", 0, "Light", 50, 0),
+    Armor("Hood", 1, "Light", 40, 20),
+    Armor("Leather caftan", 2, "Light", 30, 40)
 ]
 
 mage_armor = [
-    Armor("Szata maga", 1, "Light", 15, 20)
+    Armor("Mage's robe", 1, "Light", 15, 20)
 ]
 
 mage_spell = [
