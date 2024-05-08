85bab9a7bf9be9587becb35a226bf8f05e32cfc7
Kazume <mcwicio@gmail.com>
2024-05-08 15:44:44 +0200

    Mirror class fixed/Refactoring

--- character.py
+++ character.py
@@ -1,9 +1,9 @@
-import random
 from item import *
 
 
 class Character:
-    def __init__(self, name: str, hp: int, crit: int, armor: str, weapon: str, mana: int, pot: int):
+    def __init__(self, class_name: str, name: str, hp: int, crit: int, armor: str, weapon: str, mana: int, pot: int):
+        self.class_name = class_name
         self.name = name
         self.hp = hp
         self.hp_max = hp
@@ -21,39 +21,39 @@ class Character:
                     target.hp -= max(0, (self.weapon.dmg * 2))
                     target.hp = max(target.hp, 0)
                     print(
-                        f"{self.name} dealt CRITICAL {self.weapon.dmg * 2}dmg inflicting {max(0, (self.weapon.dmg * 2))} z {self.weapon.name}")
+                        f"{self.name} {self.class_name} dealt CRITICAL {self.weapon.dmg * 2}dmg inflicting {max(0, (self.weapon.dmg * 2))} z {self.weapon.name}")
 
                 else:
                     target.hp -= max(0, (self.weapon.dmg * 2) - target.armor.protection)
                     target.hp = max(target.hp, 0)
                     print(
-                        f"{self.name} dealt CRITICAL {self.weapon.dmg * 2}dmg inflicting {max(0, (self.weapon.dmg * 2) - target.armor.protection)} z {self.weapon.name}")
+                        f"{self.name} {self.class_name} dealt CRITICAL {self.weapon.dmg * 2}dmg inflicting {max(0, (self.weapon.dmg * 2) - target.armor.protection)} z {self.weapon.name}")
 
             else:
                 if self.weapon.weapon_type == "Magic":
                     target.hp -= max(0, self.weapon.dmg)
                     target.hp = max(target.hp, 0)
                     print(
-                        f"{self.name} dealt {self.weapon.dmg}dmg inflicting {max(0, self.weapon.dmg)} with {self.weapon.name}")
+                        f"{self.name} {self.class_name} dealt {self.weapon.dmg}dmg inflicting {max(0, self.weapon.dmg)} with {self.weapon.name}")
                 else:
                     target.hp -= max(0, self.weapon.dmg - target.armor.protection)
                     target.hp = max(target.hp, 0)
                     print(
-                        f"{self.name} dealt {self.weapon.dmg}dmg inflicting {max(0, self.weapon.dmg - target.armor.protection)} with {self.weapon.name}")
+                        f"{self.name} {self.class_name} dealt {self.weapon.dmg}dmg inflicting {max(0, self.weapon.dmg - target.armor.protection)} with {self.weapon.name}")
         else:
             print(
-                f"{self.name} miss the target")
+                f"{self.name} {self.class_name} miss the target")
 
     def potion(self, target) -> None:
-        print(f"{self.name} have currently {self.pot} potions left")
+        print(f"{self.name} {self.class_name} have currently {self.pot} potions left")
         if self.pot <= 0:
-            print(f"{self.name} do not have any left potions. Choose another action")
+            print(f"{self.name} {self.class_name} do not have any left potions. Choose another action")
             self.action(target)
         else:
-            healed = int(random.randint(1, 8))
+            healed = int(random.randint(4, 12))
             self.hp += healed
             self.pot -= 1
-            print(f"{self.name} used potion and heal {healed} and now have {self.hp}HP")
+            print(f"{self.name} {self.class_name} used potion and heal {healed} and now have {self.hp}HP")
 
     def action(self, target):
         global spell_choice
@@ -62,13 +62,13 @@ class Character:
         else:
             action_list = ["Attack", "Heal"]
         print()
-        print(f"What do you want to do {self.name}?")
+        print(f"What do you want to do {self.name} {self.class_name}?")
         for i, action in enumerate(action_list, start=1):
             print(f"{i}. {action}")
 
         while True:
             try:
-                choice = int(input(f"{self.name} Choose action"))
+                choice = int(input(f"{self.name} {self.class_name} Choose action"))
                 if 0 < choice <= len(action_list):
                     if choice == 1:
                         self.attack(target)
@@ -79,12 +79,12 @@ class Character:
                     elif choice == 3 and isinstance(self, Mage):
                         while True:
                             try:
-                                print(f"{self.name} have currently {self.mana} left")
+                                print(f"{self.name} {self.class_name} have currently {self.mana} left")
                                 print("Choose spell to cast:")
                                 for i, spell in enumerate(mage_spell, start=1):
-                                    print(f"{i}. {spell.name} ({spell.mana} mana)")
+                                    print(f"{i}. {spell.class_name} ({spell.mana} mana)")
                                 print()
-                                spell_choice = int(input(f"{self.name} choose spell to cast")) - 1
+                                spell_choice = int(input(f"{self.class_name} choose spell to cast")) - 1
                                 if 0 <= spell_choice < len(mage_spell):
                                     self.cast_spell(target, mage_spell[spell_choice])
                                     break
@@ -107,18 +107,18 @@ class Character:
 
 
 class Warrior(Character):
-    def __init__(self, name: str, armor: str, weapon: str, ) -> None:
-        super().__init__(name=name, hp=110, crit=5, armor=armor, weapon=weapon, mana=0, pot=3)
+    def __init__(self, class_name: str, name: str, armor: str, weapon: str, ) -> None:
+        super().__init__(class_name=class_name, name=name, hp=110, crit=5, armor=armor, weapon=weapon, mana=0, pot=3)
 
 
 class Archer(Character):
-    def __init__(self, name: str, armor: str, weapon: str, ) -> None:
-        super().__init__(name=name, hp=80, crit=30, armor=armor, weapon=weapon, mana=0, pot=3)
+    def __init__(self, class_name: str, name: str, armor: str, weapon: str, ) -> None:
+        super().__init__(class_name=class_name, name=name, hp=80, crit=30, armor=armor, weapon=weapon, mana=0, pot=3)
 
 
 class Mage(Character):
-    def __init__(self, name: str, armor: str, weapon: str, spell: str) -> None:
-        super().__init__(name=name, hp=70, crit=20, armor=armor, weapon=weapon, mana=150, pot=0)
+    def __init__(self, class_name: str, name: str, armor: str, weapon: str, spell: str) -> None:
+        super().__init__(class_name=class_name, name=name, hp=70, crit=20, armor=armor, weapon=weapon, mana=150, pot=0)
         self.spell = spell
 
     def cast_spell(self, target, spell):
@@ -131,39 +131,48 @@ class Mage(Character):
             else:
                 self.cast_damage_spell(target, spell)
         else:
-            print(f"{self.name}, you don't have enough mana")
+            print(f"{self.class_name}, you don't have enough mana")
             Character.action(self, target)
 
     def cast_power_up(self, spell):
         rand = random.randint(1, 3)
         self.weapon.dmg += spell.dmg + rand
-        print(f"{self.name} cast {spell.name} powering himself for {spell.dmg + rand}")
+        print(f"{self.name} {self.class_name} cast {spell.class_name} powering himself for {spell.dmg + rand}")
 
     def cast_greater_heal(self, spell):
         rand = random.randint(6, 20)
         self.hp += spell.dmg + rand
-        print(f"{self.name} cast {spell.name} healing himself for {spell.dmg + rand}")
+        print(f"{self.name} {self.class_name} cast {spell.class_name} healing himself for {spell.dmg + rand}")
 
     def cast_damage_spell(self, target, spell):
         if random.randint(1, 100) <= self.crit:
             critChance = True
         else:
             critChance = False
-        rand = random.randint(0, 12) if spell.name == "Fireball" else random.randint(2, 8)
+        rand = random.randint(0, 12) if spell.class_name == "Fireball" else random.randint(2, 8)
         dmg = (spell.dmg + rand) * 2 if critChance else spell.dmg + rand
         target.hp -= max(0, dmg)
-        print(f"{self.name} cast {spell.name} {'DEALING CRITICAL ' if critChance else 'dealing'} {dmg}dmg")
+        print(
+            f"{self.name} {self.class_name} cast {spell.class_name} {'DEALING CRITICAL ' if critChance else 'dealing'} {dmg}dmg")
 
 
 def chosen_character(player_name, characters: list):
-    for i, character in enumerate(characters, start=1):
-        print(f"{i}.  {character}")
+    for i, character_class in enumerate(characters, start=1):
+        print(f"{i}.  {character_class.__name__}")
 
     while True:
         try:
             choice = int(input(f"{player_name} choose your character")) - 1
             if 0 <= choice < len(characters):
-                return characters[choice]
+                if characters[choice] == Warrior:
+                    return Warrior(class_name="Warrior", armor=random.choice(warrior_armor),
+                                   weapon=random.choice(warrior_weapons), name=player_name)
+                elif characters[choice] == Archer:
+                    return Archer(class_name="Archer", armor=random.choice(archer_armor),
+                                  weapon=random.choice(archer_weapons), name=player_name)
+                elif characters[choice] == Mage:
+                    return Mage(class_name="Mage", armor=random.choice(mage_armor), weapon=random.choice(mage_weapon),
+                                spell=random.choice(mage_spell), name=player_name)
             else:
                 print("Wrong number choose again")
         except ValueError:
--- main.py
+++ main.py
@@ -1,47 +1,37 @@
 from character import Warrior, Archer, Mage
 import character
-from item import *
-import random
 
-characters = ["Warrior", "Archer", "Mage"]
+characters = [Warrior, Archer, Mage]
 
-classes = {
-    "Warrior": Warrior(name="Warrior", armor=random.choice(warrior_armor), weapon=random.choice(warrior_weapons)),
-    "Archer": Archer(name="Archer", armor=random.choice(archer_armor), weapon=random.choice(archer_weapons)),
-    "Mage": Mage(name="Mage", armor=random.choice(mage_armor), weapon=random.choice(mage_weapon), spell=mage_spell)
-}
-player_choose = character.chosen_character("Player 1", characters)
-player = classes.get(player_choose)
-
-enemy_choose = character.chosen_character("Player 2", characters)
-enemy = classes.get(enemy_choose)
+player = character.chosen_character("Player 1", characters)
+enemy = character.chosen_character("Player 2", characters)
 
 while True:
     if player.weapon.speed > enemy.weapon.speed:
         player.action(enemy)
         if enemy.hp == 0:
-            print(f"Player {player.name} win!!!")
+            print(f"{player.name} {player.class_name} win!!!")
             break
 
         enemy.action(player)
         if player.hp == 0:
-            print(f"Player {enemy.name} win!!!")
+            print(f"{enemy.name} {enemy.class_name} win!!!")
             break
 
     else:
         enemy.action(player)
         if player.hp == 0:
-            print(f"Player {enemy.name} win!!!")
+            print(f"{enemy.name} {enemy.class_name} win!!!")
             break
 
         player.action(enemy)
         if enemy.hp == 0:
-            print(f"Player {player.name} win!!!")
+            print(f"{player.name} {player.class_name} win!!!")
             break
 
     print()
-    print(f"hp {player.name}: {player.hp}")
-    print(f"hp {enemy.name}: {enemy.hp}")
+    print(f"HP {player.name} {player.class_name}: {player.hp}")
+    print(f"HP {enemy.name} {enemy.class_name}: {enemy.hp}")
 
     input()
 
